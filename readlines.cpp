f73e11f37de8dccd1101f11c42affcdc258304dc
natelogicgates <146137788+natelogicgates@users.noreply.github.com>
2023-10-03 14:01:42 -0700

    Update readlines.cpp

--- readlines.cpp
+++ readlines.cpp
@@ -4,8 +4,6 @@
 #include <iostream>
 #include <queue>
 
-std::queue<std::string> lineQueue;
-
 void* readlines(void* sharedData) {
     SharedData* data = static_cast<SharedData*>(sharedData);
     std::ifstream testFile(data->fileName[1]);
