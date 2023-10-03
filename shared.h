1f05ff21ccbe2b9c6ed9abe55e3ea1946a637cd3
natelogicgates <146137788+natelogicgates@users.noreply.github.com>
2023-10-03 16:11:35 -0700

    Update shared.h

--- shared.h
+++ shared.h
@@ -1,11 +1,13 @@
 // JEIKO SANTOS 824389729
 // NATHNAEL ATLAW 822655984
 #pragma once
-#include <set>
 #include <string>
-#include <queue> 
+#include <queue>
+#include <set>
 
 extern std::queue<std::string> lineQueue;
+extern pthread_mutex_t mutex;
+extern pthread_mutex_t printMutex;  // Mutex for safe printing
 
 struct SharedData {
     unsigned int numOfProgressMarks;
