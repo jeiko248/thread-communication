020a8878d1973f5bfa9da229c9b9dfdc6185e57f
natelogicgates <146137788+natelogicgates@users.noreply.github.com>
2023-10-03 16:15:23 -0700

    Update readlines.cpp

--- readlines.cpp
+++ readlines.cpp
@@ -4,19 +4,44 @@
 #include "shared.h"
 #include <fstream>
 #include <iostream>
-#include <queue>
+#include <pthread.h>
 
-void* readlines(void* sharedData) {
-    SharedData* data = static_cast<SharedData*>(sharedData);
+extern std::queue<std::string> lineQueue;
+extern pthread_mutex_t mutex;
+extern pthread_mutex_t printMutex;  // Ensure printMutex is available
+
+void* readlines(void* arg) {
+    SharedData* data = (SharedData*) arg;
     std::ifstream testFile(data->fileName[1]);
     if (!testFile.is_open()) {
-        std::cerr << "Display Unable to open <<missing_file.txt>>" << std::endl;
+        pthread_mutex_lock(&printMutex);
+        std::cerr << "Error opening test file." << std::endl;
+        pthread_mutex_unlock(&printMutex);
         return nullptr;
     }
+
     std::string line;
+    unsigned int processedLines = 0;
     while (std::getline(testFile, line)) {
+        pthread_mutex_lock(&mutex);
         lineQueue.push(line);
+        pthread_mutex_unlock(&mutex);
+        processedLines++;
+
+        // Update progress bar
+        if (processedLines % data->hashmarkInterval == 0) {
+            pthread_mutex_lock(&printMutex);
+            std::cout << "#";
+            std::cout.flush();
+            pthread_mutex_unlock(&printMutex);
+        }
     }
     testFile.close();
+
+    // Print newline and total line count after progress bar
+    pthread_mutex_lock(&printMutex);
+    std::cout << std::endl << processedLines << std::endl;
+    pthread_mutex_unlock(&printMutex);
+
     return nullptr;
 }
