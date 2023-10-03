ca45c343e2b002291f6b19c5725bd11deb7a3de9
natelogicgates <146137788+natelogicgates@users.noreply.github.com>
2023-10-03 13:31:53 -0700

    Update readvocab.cpp

--- readvocab.cpp
+++ readvocab.cpp
@@ -1,14 +1,19 @@
 #include "readvocab.h"
 #include "shared.h"
 #include <fstream>
+#include <iostream>
 
-void* readvocab(void* arg) {
-    SharedData* data = (SharedData*) arg;
+void* readvocab(void* sharedData) {
+    SharedData* data = static_cast<SharedData*>(sharedData);
     std::ifstream vocabFile(data->fileName[0]);
+    if (!vocabFile.is_open()) {
+        std::cerr << "Error opening vocabulary file." << std::endl;
+        return nullptr;
+    }
     std::string word;
     while (vocabFile >> word) {
         data->vocabulary.insert(word);
     }
     vocabFile.close();
     return nullptr;
-} 
+}
