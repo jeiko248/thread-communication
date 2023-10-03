2153da235ee49c8dba029995bd75a37112395b98
jeiko248 <83250346+jeiko248@users.noreply.github.com>
2023-10-03 14:27:27 -0700

    Update readlines.cpp

--- readlines.cpp
+++ readlines.cpp
@@ -8,7 +8,7 @@ void* readlines(void* sharedData) {
     SharedData* data = static_cast<SharedData*>(sharedData);
     std::ifstream testFile(data->fileName[1]);
     if (!testFile.is_open()) {
-        std::cerr << "Error opening test file." << std::endl;
+        std::cerr << "Display Unable to open <<missing_file.txt>>" << std::endl;
         return nullptr;
     }
     std::string line;
