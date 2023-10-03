5b358617865a676a205249018aa1b8989ed40ca8
natelogicgates <146137788+natelogicgates@users.noreply.github.com>
2023-10-03 13:39:54 -0700

    Update countvocabstrings.cpp

--- countvocabstrings.cpp
+++ countvocabstrings.cpp
@@ -1,9 +1,22 @@
 #include "countvocabstrings.h"
 #include "shared.h"
+#include <fstream>
 #include <sstream>
+#include <iostream>
+#include <pthread.h>
+#include <queue>
+
+extern std::queue<std::string> lineQueue;
+extern pthread_mutex_t mutex;
 
 void* countvocabstrings(void* arg) {
     SharedData* data = (SharedData*) arg;
+    std::ofstream outputFile("countNumOfContainedVocab.txt");
+    if (!outputFile.is_open()) {
+        std::cerr << "Error opening output file." << std::endl;
+        return nullptr;
+    }
+
     while (true) {
         pthread_mutex_lock(&mutex);
         if (lineQueue.empty()) {
@@ -23,8 +36,9 @@ void* countvocabstrings(void* arg) {
             }
         }
         if (count >= data->minNumOfVocabStringsContainedForPrinting) {
-            std::cout << line << std::endl;
+            outputFile << count << std::endl;
         }
     }
+    outputFile.close();
     return nullptr;
 }
