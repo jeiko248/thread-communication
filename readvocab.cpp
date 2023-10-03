30429ba2281462020666c758eae1096674bcc7b1
natelogicgates <146137788+natelogicgates@users.noreply.github.com>
2023-10-03 16:26:10 -0700

    Update readvocab.cpp

--- readvocab.cpp
+++ readvocab.cpp
@@ -6,38 +6,36 @@
 #include <iostream>
 #include <pthread.h>
 
-extern pthread_mutex_t printMutex;  // Ensure printMutex is available
+extern pthread_mutex_t printMutex;
 
 void* readvocab(void* arg) {
     SharedData* data = (SharedData*) arg;
     std::ifstream vocabFile(data->fileName[0]);
     if (!vocabFile.is_open()) {
-        pthread_mutex_lock(&printMutex);
         std::cerr << "Error opening vocabulary file." << std::endl;
-        pthread_mutex_unlock(&printMutex);
         return nullptr;
     }
 
-    std::string word;
     unsigned int processedWords = 0;
+    std::string word;
     while (vocabFile >> word) {
         data->vocabulary.insert(word);
         processedWords++;
 
         // Update progress bar
-        if (processedWords % data->hashmarkInterval == 0) {
+        if (data->hashmarkInterval != 0 && processedWords % data->hashmarkInterval == 0) {
             pthread_mutex_lock(&printMutex);
             std::cout << "#";
             std::cout.flush();
             pthread_mutex_unlock(&printMutex);
         }
     }
-    vocabFile.close();
 
-    // Print newline and total word count after progress bar
+    // Print newline and word count after progress bar
     pthread_mutex_lock(&printMutex);
     std::cout << std::endl << processedWords << std::endl;
     pthread_mutex_unlock(&printMutex);
 
+    vocabFile.close();
     return nullptr;
 }
